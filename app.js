1580abee4e2c583d64c902d718e0ddbecc3f8f60
paulmunyao <paul.mutiso@student.moringaschool.com>
2021-12-13 16:37:29 +0300

    Removing the clear input form

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function getName() {
-  let dayOfBirth = document.getElementById("year-input").value;
+  let dayOfBirth = document.getElementById("day-input").value;
   let monthOfBirth = Number(document.getElementById("month-input").value);
-  let yearOfBirth = Number(document.getElementById("day-input").value);
+  let yearOfBirth = Number(document.getElementById("year-input").value);
   let genders = document.getElementsByName("gender");
 }
