b3161d8ff7a5d0bf38e9c0a9121cc1f39bf3d995
paulmunyao <paulmunyao094@gmail.com>
2021-12-25 11:46:04 +0300

    Validating forms

--- app.js
+++ app.js
@@ -1,65 +1,66 @@
-function getName() {
-  let dayOfBirth = document.getElementById("day-input").value;
-  let monthOfBirth = document.getElementById("month-input").value;
-  let yearOfBirth =document.getElementById("year-input").value;
-  let genders = document.getElementsByName("gender");
-}
+let button = document.querySelector("#submit");
+button.addEventListener("click", function () {
+  let error = 0;
+  let day = document.getElementById("day-input").value;
+  let month = document.getElementById("month-input").value;
+  let year = document.getElementById("year-input").value;
+  let male = document.getElementsById("male");
+  let female = document.getElementsById("female");
+  let valid = document.getElementById("error");
 
-//Gender Validation
-function getGender () {
-      if (Male.checked){
-        return "Male"
-      }else if(Female.checked){
-        return "Female"
-      } 
+  if (day <= 0 || day > 31) {
+    error = 1;
+  } else {
+    if (month <= 0 || month > 12) {
+      error = 1;
+    } else {
+      if (year <= 0 || year > 2021) {
+        error = 1;
+      }
+    }
   }
+  if (error == 1) {
+    valid.style.color = "red";
+    valid.innerHTML = "Enter a valid date";
+  } else {
+    if (getDate(day, month, year)) {
+      let d = new Date(year + "/" + month + "/" + day);
+      
+      //Gender Validation
+      if (male.checked) {
+        return "male";
+      } else if (female.checked) {
+        return "female";
+      } else {
+        valid.style.color = "red";
+        valid.innerHTML = "Enter a valid gender";
+      }
+    }
+  }
+});
+//Date Validation
 
-  //Date Validation
-  function getDate(day,month,year){
-    if (year % 4 == 0 && month == 2){
-      if(day >29){
-        return false;
-      }else{
-        return true;
-      }
-    }
-    if (year % 4 !=0 && month != 2){
-      if(day > 28){
-        return false;
-      }else{
-        return true;
-      }
-    }
-    
-    let present = new Date();
-    let d = new Date(year +"/"+month +"/"+day);
-    if(d > present){
+function getDate(day, month, year) {
+  if (year % 4 == 0 && month == 2) {
+    if (day > 29) {
       return false;
-    }else{
+    } else {
       return true;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   }
+  if (year % 4 != 0 && month != 2) {
+    if (day > 28) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  let present = new Date();
+  let d = new Date(year + "/" + month + "/" + day);
+  if (d > present) {
+    return false;
+  } else {
+    return true;
+  }
+}
